365f1746bbbcd35d44e2bc10b1cb20b0cebd60e3
Wudong Liu <wudong.liu@gmail.com>
2026-07-26 08:47:55 +0100

    Load feedback contact styles

--- apps/web/src/components/FeedbackForm.tsx
+++ apps/web/src/components/FeedbackForm.tsx
@@ -7,6 +7,7 @@ import {
   type FeedbackType,
 } from '../lib/api/feedback'
 import { loadFeedbackContactEmail, saveFeedbackContactEmail } from '../lib/feedbackContact'
+import './FeedbackForm.css'
 
 interface FeedbackFormProps {
   variant?: 'quick' | 'full'
